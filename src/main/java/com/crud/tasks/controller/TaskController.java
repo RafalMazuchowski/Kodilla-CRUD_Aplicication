96ad1fdec1cbb18648623a1b1273ddfcbbd31ac7
RafalMazuchowski <mazuchowski.r@gmail.com>
2020-05-07 20:39:18 +0200

    Requests using PostMan (18.5)

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,6 +1,7 @@
 package com.crud.tasks.controller;
 
 import com.crud.tasks.domain.TaskDto;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,12 +18,19 @@ public class TaskController {
         return new ArrayList<>();
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "getTask/{taskId}")
+    @RequestMapping(method = RequestMethod.GET, value = "getTask")
     public TaskDto getTask(Long taskId) {
         return new TaskDto(1L, "test title", "test_content");
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask/{taskId}")
+    @RequestMapping(method = RequestMethod.GET, value = "getTask{taskId}")
+    public TaskDto getSpecificTask(@PathVariable Integer taskId) {
+        if (taskId == 3) {
+            return new TaskDto(3L, "Third title", "passive_content");
+        } else return null;
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
     public void deleteTask(Long taskId) {
 
     }
@@ -35,5 +43,10 @@ public class TaskController {
     @RequestMapping(method = RequestMethod.POST, value = "createTask")
     public void createTask(TaskDto taskDto) {
 
+    }
+
+    @RequestMapping(method = RequestMethod.PATCH, value = "patchTask")
+    public String patchTask (TaskDto taskDto){
+        return "Actualization changes...";
     }
 }
