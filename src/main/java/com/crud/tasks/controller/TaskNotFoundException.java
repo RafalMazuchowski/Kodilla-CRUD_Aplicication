8de29fb8e03e895cfd4bf414643d8fa5192f1a10
RafalMazuchowski <mazuchowski.r@gmail.com>
2020-05-19 20:37:23 +0200

    Script to launch the browser (21.4)

--- src/main/java/com/crud/tasks/controller/TaskNotFoundException.java
+++ src/main/java/com/crud/tasks/controller/TaskNotFoundException.java
@@ -1,4 +1,7 @@
 package com.crud.tasks.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "*")
 public class TaskNotFoundException extends Exception {
 }
